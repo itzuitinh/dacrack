cd9aa5c9feb1ff08eddb12a22afa0a13c91f65a9
itzuitinh <xuandieu.fetel@gmail.com>
2022-03-14 16:29:14 +0700

    Add files via upload

---
+++ max_fix_da.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LICENSE=/usr/local/directadmin/conf/license.key
+mv /usr/local/directadmin/update.tar.gz{,.bak}
+wget -O /usr/local/directadmin/update.tar.gz https://uploadwhmcs.vinahost.vn/storage/267/20220314-004403-da-1604-centos7.tar.gz
+tar -xvf /usr/local/directadmin/update.tar.gz -C /usr/local/directadmin/
+/usr/local/directadmin/directadmin p
+/usr/local/directadmin/scripts/update.sh
+if [[ -f "$LICENSE" ]]; then
+  echo "" > $LICENSE
+else
+  touch $LICENSE
+  chmod 600 $LICENSE
+  chown diradmin:diradmin $LICENSE
+fi
+service directadmin restart &
+sleep 5
+exit 1
